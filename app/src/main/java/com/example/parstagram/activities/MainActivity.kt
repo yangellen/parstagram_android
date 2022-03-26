5a749c8f38e9ee8b4e1d71e70db035dc6b4e3b06
Ellen Yang <yange@oregonstate.edu>
2022-03-25 18:19:13 -0700

    feat: user can submit w/o picture

--- app/src/main/java/com/example/parstagram/activities/MainActivity.kt
+++ app/src/main/java/com/example/parstagram/activities/MainActivity.kt
@@ -3,17 +3,48 @@ package com.example.parstagram.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.parstagram.R
 import com.parse.FindCallback
 import com.parse.ParseException
 import com.parse.ParseQuery
+import com.parse.ParseUser
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.btnSubmit).setOnClickListener{
+            val description = findViewById<EditText>(R.id.description).text.toString()
+            val user = ParseUser.getCurrentUser()
+            submitPost(description, user)
+        }
+
+        findViewById<Button>(R.id.btnTakePicture).setOnClickListener{
+
+        }
+
         queryPosts()
+    }
+
+    fun submitPost(description: String, user: ParseUser){
+        val post = Post()
+        post.setDescrption(description)
+        post.setUser(user)
+        post.saveInBackground { exception ->
+            if(exception != null){
+                Log.e(TAG,"Error while saving post")
+                exception.printStackTrace()
+                Toast.makeText(this, "Failed to submit", Toast.LENGTH_SHORT).show()
+            }else{
+                Log.e(TAG,"Successfully saved post")
+                Toast.makeText(this, "Successfully saved post", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
     //Query for all posts
