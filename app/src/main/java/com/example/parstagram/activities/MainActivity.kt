2209ac84cc705bd4fcc145450dd7d21c262e7aec
Ellen Yang <yange@oregonstate.edu>
2022-04-01 18:54:14 -0700

    feat: implement feed fragment

--- app/src/main/java/com/example/parstagram/activities/MainActivity.kt
+++ app/src/main/java/com/example/parstagram/activities/MainActivity.kt
@@ -63,7 +63,6 @@ class MainActivity : AppCompatActivity() {
         // Set default selection
         findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
 
-        queryPosts()
     }
 
     fun logout(){
@@ -77,27 +76,7 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 
-    //Query for all posts
-    fun queryPosts(){
-        // Specify which class to query
-        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
 
-        query.include(Post.KEY_USER)
-        query.findInBackground(object : FindCallback<Post>{
-            override fun done(posts: MutableList<Post>?, e: ParseException?) {
-                if (e != null){
-                    Log.e(TAG, "Error fetching posts")
-                } else {
-                    if(posts != null){
-                        for (post in posts){
-                            Log.i(TAG, "Post " + post.getDescription() + " ,username: "
-                            + post.getUser()?.username)
-                        }
-                    }
-                }
-            }
-        })
-    }
     companion object {
         const val TAG = "MainActivity"
     }
